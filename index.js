380a88af98b98bcee1d332079418b5498159362d
Error <error@Error.Error>
2022-11-30 04:08:13 +0600

    qery add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { query } = require('express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -15,6 +16,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d1gdkts.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send('unauthorized access');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
 
 async function run() {
   try {
@@ -33,13 +53,21 @@ async function run() {
     });
 
     //get cars data using email
-    app.get('/carsdata/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { email: `${id}` };
-      const cars = await carsCollection.find(query).toArray();
-      res.send(cars);
-    });
+    // app.get('/bookings', verifyJWT,  async (req, res) => {
+    app.get('/carsdata', async (req, res) => {
+      let query = {};
+      // console.log(req.query);
+      // const decodedEmail = req.decoded.email;
+      // if(email !== decodedEmail){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+      if (req.query.email) {
+        query = { email: req.query.email }
 
+      }
+      const userData = await carsCollection.find(query).toArray();
+      res.send(userData);
+    })
 
 
     app.post('/carsdata', async (req, res) => {
@@ -66,11 +94,20 @@ async function run() {
 
 
     //Get booking data
-    app.get('/bookings/:email', async (req, res) => {
-      const email = req.params.email;
-      const query = { email }
-      const user = await bookingsCollection.find(query).toArray();
-      res.send(user);
+    // app.get('/bookings', verifyJWT,  async (req, res) => {
+    app.get('/bookings', async (req, res) => {
+      let query = {};
+      // console.log(req.query);
+      // const decodedEmail = req.decoded.email;
+      // if(email !== decodedEmail){
+      //   return res.status(403).send({message: 'forbidden access'})
+      // }
+      if (req.query.email) {
+        query = { email: req.query.email }
+
+      }
+      const userData = await bookingsCollection.find(query).toArray();
+      res.send(userData);
     })
 
     // Post booked data
@@ -80,8 +117,27 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: '' })
+    })
+
     app.post('/user', async (req, res) => {
-      const user = req.body
+      const user = req.body;
+      const query = {
+        email: user.email
+      }
+      const oldUser = await userCollection.find(query).toArray();
+      if (oldUser.length) {
+        const message = "you have already login"
+        return res.send({ acknowledge: false, message })
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
@@ -115,6 +171,10 @@ async function run() {
       const query = { email }
       const user = await userCollection.findOne(query);
       res.send(user);
+    })
+
+    app.delete('/carsdata/:id', async (req, res) => {
+      const id = req.params.id;
     })
 
 
